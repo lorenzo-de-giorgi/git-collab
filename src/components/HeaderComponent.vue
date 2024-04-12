a27c9a6892518204fe348bc9b958eaa07adf8610
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-12 12:48:03 +0200

    added header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+    <header>
+        <h1>Header</h1>
+    </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'HeaderComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
