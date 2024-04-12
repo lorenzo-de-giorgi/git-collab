2644fc8583edf09ba6552c9085135f1aa9485291
Frankosviz <geom.francescopaolo@gmail.com>
2024-04-12 11:11:22 +0200

    MainComponent : Start

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Ciao, questo è un MainComponent</h1>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
