ee3e418ef7f0d90a991592cd189faa9b4b3f11f1
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-12 10:46:29 +0200

    header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1 class="text-center">Header Component</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
