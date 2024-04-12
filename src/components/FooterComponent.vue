33b113288b4aea4f00badb9b6b5459e02c17d170
lorenzoguerra01 <guerra.lo11-lg@gmail.com>
2024-04-12 11:19:41 +0200

    Add FooterComponent

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    <footer>
+        <h2>Footer</h2>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterComponent',
+
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+
+    },
+
+    mounted() {
+
+    },
+
+    created() {
+
+    },
+
+    computed: {
+
+    },
+
+    watch: {
+
+    },
+
+    unmounted() {
+
+    },
+
+    components: {
+
+    },
+
+    props: {
+
+    },
+
+    emits: {
+
+    },
+
+    directives: {
+
+    },
+
+}
+</script>
+
+<style lang="scss" scoped></style>
