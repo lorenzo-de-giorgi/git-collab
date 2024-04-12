65e49f7f7a58127e3ea3d75c6415f164e963018d
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-12 12:49:43 +0200

    add card c and card List

---
+++ src/components/CardList.vue
@@ -0,0 +1,17 @@
+<template>
+    <CardComponent />
+</template>
+
+<script>
+    import CardComponent from './CardComponent.vue'
+    export default {
+        name: 'CardList',
+        components: {
+            CardComponent,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
