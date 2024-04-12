2205ba3ad96fd219dcba3f40786e340fbc563182
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-12 13:04:22 +0200

    fixed card List

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,12 +1,18 @@
 <template>
-    <CardComponent />
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <CardComponent />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     import CardComponent from './CardComponent.vue'
     export default {
         name: 'CardList',
-        components: {
+        component: {
             CardComponent,
         }
     }
